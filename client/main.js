7eca43a733e6126e9404df2f228ecc7dd618292b
jqwang <jqwang.umich@gmail.com>
2018-12-16 01:10:46 -0500

    update main.js with login form while not successful when click submit

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import {Session} from 'meteor/session'
-import {User} from '/database/users'
+import {Users} from '/database/users'
 
 import './main.html';
 
@@ -15,9 +15,70 @@ Template.main.helpers({
   }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.loginForm.onCreated(function() {
+	localStorage.setItem('loginError', 'No error');
+});
+
+Template.loginForm.helpers({
+  getError(){
+    return localStorage.getItem('loginError');
+  }
+});
+
+Template.loginForm.events({
+  'submit #loginForm': function(event) {
+		// event.preventDefault();
+
+		var newUsername = event.target.loginName.value;
+
+		if(newUsername) {
+
+			var userExists = Users.find({ user: newUsername }).count();
+      console.log("user name ",userExists);
+			if(userExists) {
+			  localStorage.setItem('currentUser', newUsername);
+			}else {
+        localStorage.setItem('loginError',"Error: Username does not exist");
+			}
+		}else {
+			localStorage.setItem('loginError',"Error: Please enter a username");
+		}
+	},
+
+});
+
+// ---------------------------------------------------------------------------------
+Template.signupForm.onCreated(function() {
+	this.signupFormError = new ReactiveVar("No error");
+});
+
+Template.signupForm.helpers({
+  getError(){
+    console.log("error message ",Template.instance().loginFormError.get());
+    return Template.instance().loginFormError.get();
+
+  }
+});
+
+Template.signupForm.events({
+  'submit #loginForm': function(event) {
+		event.preventDefault();
+
+		var newUsername = event.target.loginName.value;
+
+		if(newUsername) {
+
+			var userExists = Users.find({ user: newUsername }).count();
+      console.log("user name ",userExists);
+			if(userExists) {
+				Session.set('currentUser', newUsername);
+			}else {
+        Template.instance().loginFormError.set("Error: Username does not exist");
+        $('#login-error').css('display', 'block');
+			}
+		}else {
+			Template.instance().loginFormError.set("Error: Please enter a username");
+		}
+	},
+
 });
